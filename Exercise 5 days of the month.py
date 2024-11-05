71fa72d4251cfa2d1850fb443a8dd9672d719eac
JheiromAngelion28 <jheirom.amp@gmail.com>
2024-11-05 12:22:26 +0400

    Add files via upload

---
+++ Exercise 5 days of the month.py
@@ -0,0 +1,29 @@
+
+dyear = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+
+dmonth = int(input("Please enter the number of the month: "))
+if 1 <= dmonth <= 12:
+    if dmonth == 2:
+        leap_year = (input("Is the year considered a leap year? yes/no: ")).lower()
+        if leap_year == "yes":
+            print("The Month of February has 29 days")
+        else:
+            print("The month Of February has 28 days")
+    else:
+        print(f"This month has {dyear[dmonth]} days~!")
+
+else:
+    print(f"invalid number")
